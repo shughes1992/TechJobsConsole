dde13ed857aba4f859b46964e6b26f3d1f8509d9
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-04-27 21:26:00 -0400

    added job line separators

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -119,26 +119,34 @@ namespace TechJobsConsole
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
 
-            foreach (Dictionary<string, string> searchResults in someJobs)
+            foreach (Dictionary<string, string> keyValuePairs in someJobs)
             {
-                foreach (Dictionary<string, string> keyValuePairs in someJobs)
+
+
+                foreach (Dictionary<string, string> job in someJobs)
+
                 {
-                    Console.WriteLine($"Name: {keyValuePairs["name"]}");
-                    Console.WriteLine($"Employer: {keyValuePairs["employer"]}");
-                    Console.WriteLine($"Location: {keyValuePairs["location"]}");
-                    Console.WriteLine($"Position: {keyValuePairs["position type"]}");
-                    Console.WriteLine($"Core Comp: {keyValuePairs["core competency"]}");
+
+
+                    Console.WriteLine($"Name: {job["name"]}");
+                    Console.WriteLine($"Employer: {job["employer"]}");
+                    Console.WriteLine($"Location: {job["location"]}");
+                    Console.WriteLine($"Position: {job["position type"]}");
+                    Console.WriteLine($"Core Comp: {job["core competency"]}");
+
+                    Console.WriteLine("┈┈┈┈┈┈┈┈┈┈┈┈┈┈❀┈┈┈┈┈┈┈┈┈┈┈┈┈┈");
 
                 }
 
-
-
-
-                //will need to iterate over the LIST of jobs ^
-                //think instead about creating a nested loop
-                //to iterate over each dictionary key. 
-                //Console.WriteLine("PrintJobs is not implemented yet");
             }
+
         }
+
+        //will need to iterate over the LIST of jobs ^
+        //think instead about creating a nested loop
+        //to iterate over each dictionary key. 
+        //Console.WriteLine("PrintJobs is not implemented yet");
+
+
     }
 }
